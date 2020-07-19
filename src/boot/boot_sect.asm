59eca61879f203cc5e6908603cd4683d023ca064
Gabriel Zsigmond <gaazsig@gmail.com>
2020-07-18 23:05:38 -0300

    Organising OS Code Base

---
+++ src/boot/boot_sect.asm
@@ -0,0 +1,50 @@
+[org 0x7c00]
+KERNEL_OFFSET equ 0x1000 
+
+
+    mov [BOOT_DRIVE], dl 
+    mov bp, 0x9000 
+    mov sp, bp
+    
+    mov bx, MSG_REAL_MODE 
+    call print 
+    call print_nl
+
+    call load_kernel 
+    call switch_to_pm 
+
+    jmp $
+
+%include "boot/print_string16.asm"
+%include "boot/load_disk.asm"
+%include "boot/gdt.asm"
+%include "boot/print_string32.asm"
+%include "boot/switch_to_pm.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL 
+    call print
+    call print_nl
+    
+    mov bx, KERNEL_OFFSET 
+    mov dh, 2
+    mov dl, [BOOT_DRIVE] 
+    call load_disk 
+    ret
+
+[bits 32]
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE 
+    call print_string_pm 
+    call KERNEL_OFFSET 
+    jmp $
+
+
+BOOT_DRIVE db 0
+MSG_REAL_MODE db "Started in 16 - bit Real Mode", 0
+MSG_PROT_MODE db "Successfully landed in 32 - bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory.", 0
+
+times 510 -($-$$) db 0
+dw 0xaa55
